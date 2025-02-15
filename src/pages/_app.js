9473d09c718f5fb8f62fffcbdeed112d02ae84e8
Jasper Gozum <gozumjasper@gmail.com>
2025-02-16 04:42:48 +0800

    hairy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,8 +6,14 @@ export default function App({ Component, pageProps }) {
     <>
     <Head>
         <title>Hairy</title>
-        <meta name="description" content="This is my awesome website" />
+        <meta name="description" content="A hairy creature named Hairy" />
         <link rel="icon" href="/favicon.ico" />
+
+        <meta property="og:title" content="Hairy" />
+        <meta property="og:description" content="A hairy creature named Hairy" />
+        <meta property="og:image" content="https://hairy4.vercel.app/img/hairy2.png" />
+        <meta property="og:url" content="https://hairy4.vercel.app/" />
+        <meta property="og:type" content="website" />
       </Head>
       <Header />
       <Component {...pageProps} />
